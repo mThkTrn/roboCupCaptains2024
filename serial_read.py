a4a68814aaf3f4da75493a14fd0b72ef8e1eaeee
mThkTrn <128846821+mThkTrn@users.noreply.github.com>
2024-07-14 23:26:40 -0400

    Update serial_read.py

--- serial_read.py
+++ serial_read.py
@@ -10,4 +10,16 @@ def read_serial():
 ballang, ballsize, goal1ang, goal1size, goal2ang, goal2size = -1, -1, -1, -1, -1, -1
 
 def parse_serial():
-    ballang, ballsize, goal1ang, goal1size, goal2ang, goal2size = [int(k) for k in read_serial().split(",")]
+    global ballang
+    global ballsize
+    global goal1ang
+    global goal2size
+    global goal2ang
+    global goal2size
+    cam_in = read_serial()
+    #print("--")
+    print(cam_in[:-1])
+    if len(cam_in.split(",")) == 6:
+        #print("updating values...")
+        ballang, ballsize, goal1ang, goal1size, goal2ang, goal2size = [float(k) for k in cam_in.split(",")]
+        #print(ballsize)
